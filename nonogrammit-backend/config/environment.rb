b9ed1e53ebdf9d806cf919f91b7e2c4753617b1f
Eric Clark <eclark0013@gmail.com>
2020-04-22 15:29:57 -0400

    Add raw puzzle database to controller

--- nonogrammit-backend/config/environment.rb
+++ nonogrammit-backend/config/environment.rb
@@ -1,6 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 require 'open-uri'
+require 'httparty'
+require 'nokogiri'
+require 'web-console'
 
 
 # Initialize the Rails application.
--- nonogrammit-frontend/src/index.js
+++ nonogrammit-frontend/src/index.js
@@ -3,3 +3,13 @@ const BACKEND_URL = 'http://localhost:3000/';
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
+let testButton
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(document.querySelector("#test-button").innerHTML)
+    let testButton = document.querySelector("#test-button")
+    testButton.addEventListener("click", () => {
+        testButton.innerHTML = "Oh, no I've been clicked!"
+    })
+})
+
