43d3995d0c08bd07264acb2f719d83c8164ef904
Eric Clark <eclark0013@gmail.com>
2020-04-22 16:53:13 -0400

    Display puzzle number after each new puzzle click

--- nonogrammit-frontend/src/index.js
+++ nonogrammit-frontend/src/index.js
@@ -13,12 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .then((data) => {
         let puzzleNumber = Math.floor(Math.random()*20)
-        
         let singlePuzzle = data["rawPuzzleDatabase"][puzzleNumber]
+        displayPuzzleNumber(puzzleNumber)
         console.log(singlePuzzle);
       });
     })
-    
-
 })
 
+function displayPuzzleNumber(puzzleNumber){
+  if (document.querySelector("#puzzle-number-display")){
+    document.querySelector("#puzzle-number-header").innerHTML = `Puzzle #${puzzleNumber}`
+  }
+  else{
+    let puzzleNumberDiv = document.createElement("div")
+    puzzleNumberDiv.id = "puzzle-number-display"
+    let puzzleNumberHeader = document.createElement("h2")
+    puzzleNumberHeader.id = "puzzle-number-header"
+    puzzleNumberDiv.appendChild(puzzleNumberHeader)
+    puzzleNumberHeader.innerHTML = `Puzzle #${puzzleNumber}`
+    document.querySelector("#title").after(puzzleNumberDiv)
+  }
+}
