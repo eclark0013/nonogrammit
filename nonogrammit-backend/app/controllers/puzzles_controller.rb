14f6625ea7e446fa96a6a2f47c81c2f8611cbd1f
Eric Clark <eclark0013@gmail.com>
2020-04-24 15:46:57 -0400

    Add fast json_api gem and puzzle serializer

--- nonogrammit-backend/app/controllers/puzzles_controller.rb
+++ nonogrammit-backend/app/controllers/puzzles_controller.rb
@@ -1,4 +1,4 @@
-class PuzzleController < ApplicationController
+class PuzzlesController < ApplicationController
     def puzzleInfo
         render json: { rawPuzzleDatabase: 
             ["8.4.2/13/3.10.3/3.1.2.7/2.8/1.1.8/1.2.3.1.2/4.8/1.4.8/16/3.3.8.1/3.5.1.4.1/4.4.8.1/11.4.1/6.1.3.2/2.4.4.3/2.4.3.2/2.8.3/1.3.1.3.2/6.1.1.2/6.3/16.1/2.4.4.1/1.3.3.2/1.3.1.2.3/4.9/4.8/4.5.2/2.4/3.3.4/3.6.4/3.3.4.1.2/4.3.6/2.2.6.6/3.3.1.1.3.1/3.3.10/3.1.3.4/3.4.4.4/8.1.2.2/2.4.1.1.1/6.1.1/1.2.1.4/1.11.8/11.2.3/11.6/3.7.1/9.4/4.3.2.2/9.3.2/5.5.2",
@@ -24,6 +24,8 @@ class PuzzleController < ApplicationController
         }
     end
 
-    def show
+    def index
+        puzzles = Puzzle.all
+        render json: PuzzleSerializer.new(puzzles)
     end
 end
--- nonogrammit-backend/config/routes.rb
+++ nonogrammit-backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
-  get '/puzzleInfo', to: 'puzzle#puzzleInfo'
+  get '/puzzleInfo', to: 'puzzles#puzzleInfo'
   resources :users, only: [:create, :index]
+  resources :puzzles
 end
